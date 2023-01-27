c7b518b45f21000dfb34f91b725c979d56746dcb
donna booker <donnabooker444@gmail.com>
2023-01-27 17:15:16 -0500

    slicesCreated

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import { campsitesReducer } from "../features/campsites/campsitesSlice";
+import { commentsReducer } from "../features/comments/commentsSlice";
+import { partnersReducer } from "../features/partners/PartnersSlice";
+import { promotionsReducer } from "../features/promotions/promotionsSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    promotions: promotionsReducer,
+    partners: partnersReducer,
+    comments: commentsReducer,
+    campsites: campsitesReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
 });
